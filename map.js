d790b75c67929cc3e5efb4a258562da14de7d764
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2023-05-25 13:27:34 -0600

    some code for the google map api

--- map.js
+++ map.js
@@ -1,1 +1,16 @@
 // map javascript
+
+// get location place holder
+
+    // let map;
+
+// Here is where to implement map once created
+
+    // function initialize_map() {}
+
+    // function get_location() {}
+
+
+    // localStorage.setItem("user_location", current_location)
+
+    // console.log("Location saved:", current_location)
