c21655ea802ea8fdd35505c1073aa58f84530417
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2023-05-25 17:37:48 -0600

    I think the marker js collects the data and puts it into storage?

--- marker.js
+++ marker.js
@@ -1,2 +1,13 @@
 // Collect data from User Input
 
+function location_information() {
+    const locationNameEl = document.querySelector("#location");
+    const featureNameEl = document.querySelector("#features");
+    const infoNameE1 = document.querySelector("#notes");
+
+    localStorage.setItem("locationName", locationNameEl.value);
+    localStorage.setItem("featureName", featureNameEl.value);
+    localStorage.setItem("infoName", infoNameEl.value);
+
+    window.location.href = "map.html";
+}
