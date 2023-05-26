0e2c6c18bb2be893e49958f219e3b3c5c1cade16
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2023-05-25 20:42:59 -0600

    table can update based on the info the user gives

--- alldata.js
+++ alldata.js
@@ -1,37 +1,59 @@
 // add data into the data table
 
-document.addEventListener("DOMContentLoaded", function() {  
-    // const playerName = document.getElementById("user_name");
-    // const storedUserName = localStorage.getItem("userName");
-    // playerName.textContent = storedUserName;
+// document.addEventListener("DOMContentLoaded", function() {  
 
-    // const locationName = document.getElementById("location");
-    // const storedlocationName = document.getItem("locationName");
+//     const storedLocation = localStorage.getItem("locationName");
+//     const storedFeatures = localStorage.getItem("featureName");
+//     const storedNotes = localStorage.getItem("infoName");
+//     const storedUser = localStorage.getItem("userName");
+//     const storedDate = "4/23/2020";
 
-    // const featureName = document.getElementById("features");
-    // const storedfeatureName = document.getItem("locationName");
-
-    // const infoName = document.getElementById("notes");
-    // const storedinfoName = document.getItem(infoName);
-
-    const storedLocation = localStorage.getItem("locationName");
-    const storedFeatures = localStorage.getItem("featureName");
-    const storedNotes = localStorage.getItem("infoName");
-    const storedUser = localStorage.getItem("userName");
+//     document.getElementById("location").textContent = storedLocation;
+//     document.getElementById("features").textContent = storedFeatures;
+//     document.getElementById("notes").textContent = storedNotes;
+//     document.getElementById("user").textContent = storedUser;
+//     document.getElementById("date").textContent = storedDate;
+// });
 
 
+function loadScores() {
+    let infos = [];
+    const infosText = localStorage.getItem('infos');
+    if (infosText) {
+      infos = JSON.parse(infosText);
+    }
+  
+    const tableBodyEl = document.querySelector('#infos');
+  
+    if (infos.length) {
+      for (const [i, info] of infos.entries()) {
+        const locationTdEl = document.createElement('td');
+        const featuresTdEl = document.createElement('td');
+        const notesTdEl = document.createElement('td');
+        const userTdEl = document.createElement('td')
+        const dateTdEl = document.createElement('td');
 
-    const storedDate = "4/23/2020";
-
-
-
-    document.getElementById("location").textContent = storedLocation;
-    document.getElementById("features").textContent = storedFeatures;
-    document.getElementById("notes").textContent = storedNotes;
-    document.getElementById("user").textContent = storedUser;
-    // locationName.textContent = storedlocationName;
-    // featureName.textContent = storedfeatureName;
-    // infoName.textContent = storedinfoName;
-    // playerName.textContent = storedUserName;
-    document.getElementById("date").textContent = storedDate;
-});
+        const storedDate = "4/23/2020";
+  
+        locationTdEl.textContent = info.location;
+        featuresTdEl.textContent = info.features;
+        notesTdEl.textContent = info.notes;
+        userTdEl.textContent = info.user;
+        dateTdEl.textContent = storedDate;
+        
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(locationTdEl);
+        rowEl.appendChild(featuresTdEl);
+        rowEl.appendChild(notesTdEl);
+        rowEl.appendChild(userTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+  }
+  
+  loadScores();
--- marker.js
+++ marker.js
@@ -4,10 +4,28 @@ function location_information() {
     const locationNameEl = document.querySelector("#location");
     const featureNameEl = document.querySelector("#features");
     const infoNameEl = document.querySelector("#notes");
+    const userNameEl = localStorage.getItem('userName');
 
     localStorage.setItem("locationName", locationNameEl.value);
     localStorage.setItem("featureName", featureNameEl.value);
     localStorage.setItem("infoName", infoNameEl.value);
 
+    let infos = [];
+    const infosText = localStorage.getItem('infos');
+    if (infosText) {
+      infos = JSON.parse(infosText);
+    }
+
+    const newInfo = {
+        location: locationNameEl.value,
+        features: featureNameEl.value,
+        notes: infoNameEl.value,
+        user: userNameEl.value
+    };
+
+    infos.push(newInfo);
+
+    localStorage.setItem('infos', JSON.stringify(infos));
+
     window.location.href = "alldata.html";
-}
+  }
