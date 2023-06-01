246daad2512a23beed7fdd46809601535cc1ff8b
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2023-06-01 16:02:45 -0600

    login feature

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json())
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/test',(req, res) => {
+    res.send("Hello")
+})
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
+  
+app.listen(port, () => {
+console.log(`Listening on port ${port}`);
+});
+
+// const url = " https://api.quotable.io/random";
+// fetch(url)
+//   .then((x) => x.json())
+//   .then((response) => {
+//     document.querySelector("pre").textContent = JSON.stringify(
+//       response,
+//       null,
+//       "  "
+//     );
+//   });
--- public/login.js
+++ public/login.js
@@ -1,6 +1,11 @@
-function login() {
+async function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
+
+    const response = await fetch("/api/test")
+    const temp = await response.json()
+    console.log(temp)
+
     window.location.href = "map.html";
   }
   
